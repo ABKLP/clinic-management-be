36a15d05ef1d891f17f558dae6784544f7df6270
Lanh Hoang <lanhhoang@users.noreply.github.com>
2023-03-09 13:11:16 -0500

    Add Appointment router

--- config/app.js
+++ config/app.js
@@ -9,6 +9,7 @@ let cors = require("cors");
 
 var indexRouter = require("../routes/index");
 var usersRouter = require("../routes/users");
+var appointmentRouter = require("../routes/appointments");
 
 var errorHandler = require("./error-handler");
 
@@ -27,6 +28,7 @@ app.use(passport.initialize());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/appointments", appointmentRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
